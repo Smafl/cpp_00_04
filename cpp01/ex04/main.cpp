f47a88d573ed2b34b1d506bc1b72cef6f8b31452
Lena <lennykulichkova@gmail.com>
2023-10-12 22:29:37 +0200

    cpp01/ex04 in process

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,13 +1,42 @@
 
+#include <string>
 #include <fstream>
 #include <iostream>
+#include <sstream>
 
-int replace(const std::string filename, const std::string s1, const std::string s2)
+int replace(const char *filename, const std::string &s1, const std::string &s2)
 {
+	std::ifstream infile(filename);
+	if (infile.rdstate() & std::ifstream::failbit)
+	{
+		std::cerr << "File cannot be opened" << std::endl;
+		return EXIT_FAILURE;
+	}
 	if (s1.empty())
 	{
-		std::cout << "Nothing to replace." << std::endl;
+		std::cerr << "Nothing to replace." << std::endl;
 		return EXIT_FAILURE;
+	}
+	std::stringstream buffer;
+	buffer << infile.rdbuf();
+	std::string file_content(buffer.str());
+	std::string name(filename);
+	name += ".replace";
+	std::ofstream new_file(name);
+	if (infile.rdstate() & std::ifstream::failbit)
+	{
+		std::cerr << "New file cannot be opened" << std::endl;
+		return EXIT_FAILURE;
+	}
+	std::string::size_type i(0);
+	while (true)
+	{
+		std::string::size_type j(0);
+		i = file_content.find(s1, i);
+		if (i != std::string::npos)
+		{
+
+		}
 	}
 	return EXIT_SUCCESS;
 }
@@ -16,7 +45,7 @@ int main(int argc, const char **argv)
 {
 	if (argc != 4)
 	{
-		std::cout << "Required arguments: ./replace, filename, " \
+		std::cerr << "Required arguments: ./replace, filename, " \
 					 "string to replace, string to replace with" << std::endl;
 		return EXIT_FAILURE;
 	}
