1c3c02b929d7dffc7c9bdd67d4904f8b19daee15
Elena Kulichkova <ekulichk@3-B-4.42heilbronn.de>
2023-10-16 23:29:51 +0200

    finish cpp01/ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -11,11 +11,11 @@ int replace(const char *filename, const std::string &s1, const std::string &s2)
 	std::ifstream infile(filename);
 
 	// if the file cannot be opened, the stream's failbit flag is set:
-	if (infile.rdstate() & std::ifstream::failbit)
-	{
-		std::cerr << "File cannot be opened" << std::endl;
-		return EXIT_FAILURE;
-	}
+//	if (infile.rdstate() & std::ifstream::failbit)
+//	{
+//		std::cerr << "File cannot be opened" << std::endl;
+//		return EXIT_FAILURE;
+//	}
 	if (s1.empty())
 	{
 		std::cerr << "Nothing to replace." << std::endl;
@@ -35,11 +35,11 @@ int replace(const char *filename, const std::string &s1, const std::string &s2)
 
 	// create a new file and check if it's open
 	std::ofstream new_file(name);
-	if (infile.rdstate() & std::ifstream::failbit)
-	{
-		std::cerr << "New file cannot be opened" << std::endl;
-		return EXIT_FAILURE;
-	}
+//	if (infile.rdstate() & std::ifstream::failbit)
+//	{
+//		std::cerr << "New file cannot be opened" << std::endl;
+//		return EXIT_FAILURE;
+//	}
 
 	std::string::size_type len_s1(s1.length());
 	std::string::size_type len_s2(s2.length());
@@ -47,6 +47,8 @@ int replace(const char *filename, const std::string &s1, const std::string &s2)
 	while (i != std::string::npos)
 	{
 		i = file_content.find(s1, i);
+		if (i == std::string::npos)
+			break;
 		file_content.erase(i, len_s1);
 		file_content.insert(i, s2);
 		i += len_s2;
@@ -63,5 +65,6 @@ int main(int argc, const char **argv)
 					 "string to replace, string to replace with" << std::endl;
 		return EXIT_FAILURE;
 	}
-	return replace(argv[1], argv[2], argv[3]);
+	replace(argv[1], argv[2], argv[3]);
+	return EXIT_SUCCESS;
 }
