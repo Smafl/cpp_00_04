0eb33a527f0129ae89e30cd432850ea37ea52556
Elena Kulichkova <ekulichk@3-G-11.42heilbronn.de>
2023-10-18 15:57:13 +0200

    start cpp01/ex06

---
+++ cpp01/ex06/main.cpp
@@ -0,0 +1,13 @@
+
+#include "Harl.hpp"
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	Harl harl;
+	if (argc != 2)
+	{
+		std::cout << "[ probably some whining.. ]" << std::endl;
+	}
+	return 0;
+}
