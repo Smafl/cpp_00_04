8d18b28df32064e3a1b3a56cfc825b43864b80f3
Elena Kulichkova <ekulichk@2-H-8.42heilbronn.de>
2023-10-27 19:13:19 +0200

    finish cpp03/ex00

--- cpp03/ex00/ClapTrap.hpp
+++ cpp03/ex00/ClapTrap.hpp
@@ -7,16 +7,28 @@
 class ClapTrap {
 private:
 	std::string		_name;
-	unsigned int	_hitPoints;
+	unsigned int	_hitPoints; // represent the health
 	unsigned int	_energyPoints;
 	unsigned int	_attackDamage;
 public:
 	ClapTrap();
-	ClapTrap(std::string &name);
+	ClapTrap(const std::string &name);
+	ClapTrap(const ClapTrap &other);
+	ClapTrap &operator=(const ClapTrap &other);
 	~ClapTrap();
-	void attack(const std::string& target);
+
+//	actions
+	void attack(const std::string &target);
 	void takeDamage(unsigned int amount);
 	void beRepaired(unsigned int amount);
+
+//	setters
+	void setAttackDamage(unsigned int damage);
+
+//	getters
+	std::string getName() const;
+	unsigned int getHitPoints() const;
+	unsigned int getAttackDamage() const;
 };
 
 #endif // CLAPTRAP_HPP
--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -4,6 +4,17 @@
 int main()
 {
 	ClapTrap bob("Bob");
+	bob.setAttackDamage(3);
+	ClapTrap unbob;
+	bob.attack(unbob.getName());
+	unbob.takeDamage(bob.getAttackDamage());
+	unbob.takeDamage(bob.getAttackDamage());
+	unbob.takeDamage(bob.getAttackDamage());
+	unbob.takeDamage(bob.getAttackDamage());
+	unbob.beRepaired(bob.getAttackDamage());
+	ClapTrap copy(bob);
+	ClapTrap assign;
+	assign = unbob;
 
 	return 0;
 }
