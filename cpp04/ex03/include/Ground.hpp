7892fb21e03546d4beb46c31e6ea5555755e94c0
Lena <lennykulichkova@gmail.com>
2023-10-31 20:09:58 +0100

    make beauty in cpp04/ex03/include/Ground.hpp

--- cpp04/ex03/include/Ground.hpp
+++ cpp04/ex03/include/Ground.hpp
@@ -4,18 +4,25 @@
 
 #include "AMateria.hpp"
 
+// the Singleton pattern is a design pattern used to ensure
+// that a class has only one instance and provides a global
+// point of access to that instance. This is particularly
+// useful when you want to restrict the instantiation of a
+// class to a single object, often used for managing global
+// resources, configurations, or services
 class Ground {
-public:
-    static Ground &getInstance(); // don't need an object
-    ~Ground();
-
-    void takeMateria(AMateria *m);
 private:
     AMateria *_array[50];
     unsigned int _counter;
+
+//  only private
     Ground();
     Ground(const Ground &other);
     Ground &operator=(const Ground &other);
+public:
+    ~Ground();
+    static Ground &getInstance(); // don't need an object
+    void takeMateria(AMateria *m);
 };
 
 #endif // GROUND_HPP
